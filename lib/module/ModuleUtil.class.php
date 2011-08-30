8734fbb0e75674542874704e36d4895bb18dfb9d
TimWolla <timwolla@arcor.de>
2011-08-30 19:00:58 +0200

    Slight reformat

--- lib/module/ModuleUtil.class.php
+++ lib/module/ModuleUtil.class.php
@@ -14,25 +14,21 @@ class ModuleUtil extends Module {
 			$bot->queue('/whisper '.$bot->message['usernameraw'].', /color '.substr($matches[0][0], 7).' '.substr($matches[0][count($matches[0])-1], 7));
 		}*/
 		else if ($bot->message['text'] == '!info') {
-			if (Core::compareLevel($bot->lookUpUserID(), 'util.info')) {
-				$formatFileSize = function ($size) {
-					$types = array('b', 'KiB', 'MiB', 'GiB', 'TiB');
-					$type = 0;
-					while ($size > 1024) {
-						$type++;
-						$size /= 1024;
-					} 
-					return round($size, 4).' '.$types[$type];
-				};
-				$bot->queue(Core::language()->util_information.':');
-				$bot->queue(Core::language()->util_since.": ".date('d.m.Y H:i:s', TIME));
-				$bot->queue(Core::language()->util_got.": ".$bot->messageCount.' ('.round($bot->messageCount / (time() - TIME) * 60, 4).'/m)');
-				$bot->queue(Core::language()->util_sent.": ".$bot->sendCount.' ('.round($bot->sendCount / (time() - TIME) * 60, 4).'/m)');
-				$bot->queue('Speicherauslastung: '.$formatFileSize(memory_get_usage()).'; Hoechststand: '.$formatFileSize(memory_get_peak_usage()));
-			}
-			else {
-				$bot->denied();
-			}
+			if (!Core::compareLevel($bot->lookUpUserID(), 'util.info')) return $bot->denied();
+			$formatFileSize = function ($size) {
+				$types = array('b', 'KiB', 'MiB', 'GiB', 'TiB');
+				$type = 0;
+				while ($size > 1024) {
+					$type++;
+					$size /= 1024;
+				}
+				return round($size, 4).' '.$types[$type];
+			};
+			$bot->queue(Core::language()->util_information.':');
+			$bot->queue(Core::language()->util_since.": ".date('d.m.Y H:i:s', TIME));
+			$bot->queue(Core::language()->util_got.": ".$bot->messageCount.' ('.round($bot->messageCount / (time() - TIME) * 60, 4).'/m)');
+			$bot->queue(Core::language()->util_sent.": ".$bot->sendCount.' ('.round($bot->sendCount / (time() - TIME) * 60, 4).'/m)');
+			$bot->queue('Speicherauslastung: '.$formatFileSize(memory_get_usage()).'; Hoechststand: '.$formatFileSize(memory_get_peak_usage()));
 		}
 	}
 }
