0c6eb9d8fb4883ad953db659a89d0f4247bc8244
Tim Düsterhus <timwolla@googlemail.com>
2012-07-03 20:26:52 +0300

    Fixing dice

--- lib/module/ModuleToys.class.php
+++ lib/module/ModuleToys.class.php
@@ -23,15 +23,12 @@ class ModuleToys extends Module {
 		if (substr($bot->message['text'], 0, 5) == '!dice') {
 			$command = substr($bot->message['text'], 5);
 			$command = explode('d', $command, 2);
-			$command[0] = min(10, intval($command[0]));
-			if (count($command) > 1) {
-				while ($command[0]--) {
-					$results[] = rand(1, intval($command[1]));
-				}
+			$command[0] = max(10, intval($command[0]));
+			
+			while ($command[0]-- > 0) {
+				$results[] = rand(1, intval(count($command) > 1 ? $command[1] : 6));
 			}
-			else {
-				 $results[] = rand(1,6);
-			}
+			
 			sort($results);
 			$bot->queue(implode(', ', $results).' wurde von '.$bot->message['usernameraw'].' gewuerfelt');
 		}
